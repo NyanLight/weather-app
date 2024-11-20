65dfcd264a1020d02466513e0b681a28dd863077
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-11-20 15:18:11 +0400

    complete forecast function

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const API_KEY = "DGY8RG748CBYHNVJKCJSKWMHL";
 
-async function getData(city = "Moscow") {
+async function getData(city = "Moscow", unit = "metric") {
   const data = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}&unitGroup=${unit}`
   );
-  console.log(data.json());
+  return data.json();
 }
 
-getData();
+async function getForecast(city) {
+  const data = await getData(city);
+  [currentTemp, currentFeels, currentIcon] = getCurrentData(data);
+  [todayMin, todayMax, todayIcon] = getDailyData(0, data);
+  [tomorrowMin, tomorrowMax, tomorrowIcon] = getDailyData(1, data);
+  return {
+    current: [currentTemp, currentFeels, currentIcon],
+    today: [todayMin, todayMax, todayIcon],
+    tomorrow: [tomorrowMin, tomorrowMax, tomorrowIcon],
+  };
+}
+
+function getCurrentData(data) {
+  const today = new Date();
+  const currentHour = today.getHours();
+  for (const hour of data.days[0].hours) {
+    if (currentHour == hour.datetime.slice(0, 2)) {
+      return [hour.temp, hour.feelslike, hour.icon];
+    }
+  }
+}
+
+function getDailyData(index, data) {
+  return [
+    data.days[index].tempmin,
+    data.days[index].tempmax,
+    data.days[index].icon,
+  ];
+}
+
+getForecast();
